6656f885d380c43c03780a5fa650873c63b05893
Kacper Drupisz <kacper.drupisz@student.witkac.pl>
2021-01-17 00:32:11 +0000

    Project Chapter 3 The Collatz Sequence

---
+++ 3 Functions/TheCollatzSequence.py
@@ -0,0 +1,27 @@
+from typing import Sequence
+
+
+# Chapter 3
+# Project: the Collatz Sequence
+
+def collatz(number):
+    try:
+        # If number is even
+        if number % 2 == 0:
+            return number // 2
+        # if number is odd
+        else:
+            return 3 * number + 1
+    except ValueError:
+        print("Number is noninteger")
+
+
+print("Enter number:")
+userNumber = int(input())
+
+# Using this sequence, you’ll arrive at 1
+while(True):
+    userNumber = collatz(userNumber)
+    print(userNumber)
+    if(userNumber == 1):
+        break
